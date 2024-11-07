ef0b7ebbdd88a5eafdb1e9d02eae3372f2e71483
Wanderley Drumond <wanderley.drumond@gmail.com>
2024-11-07 19:07:55 +0000

    Project frontend configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {RouterOutlet} from '@angular/router';
+import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import {faPlayCircle} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, FontAwesomeModule],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+    title = 'frontend';
+    private faIconLibrary = inject(FaIconLibrary);
+
+    ngOnInit(): void {
+        this.initFontAwesome();
+    }
+
+    private initFontAwesome() {
+        this.faIconLibrary.addIcons(...fontAwesomeIcons);
+    }
+
+    protected readonly faPlayCircle = faPlayCircle;
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,6 @@
+import {IconDefinition} from "@fortawesome/fontawesome-svg-core";
+import {faCirclePlay} from "@fortawesome/free-solid-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+    faCirclePlay
+]
